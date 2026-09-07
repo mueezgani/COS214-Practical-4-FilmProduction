c1c730b0069e843aad49d197d2a70cf30fa1acdc
Mueez Gani <u24983439@tuks.co.za>
2026-09-07 18:33:52 +0200

    Add Blocked state header

---
+++ include/Blocked.h
@@ -0,0 +1,17 @@
+#ifndef BLOCKED_H
+#define BLOCKED_H
+
+#include "TaskState.h"
+
+class Blocked : public TaskState
+{
+public:
+    void start(ProductionTask* task) override;
+    void block(ProductionTask* task) override;
+    void resume(ProductionTask* task) override;
+    void complete(ProductionTask* task) override;
+    void cancel(ProductionTask* task) override;
+    std::string getName() const override;
+};
+
+#endif
